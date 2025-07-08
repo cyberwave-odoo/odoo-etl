a88d8c7ebc3272b968ade44918cfbf37136fe76e
jer.dewandre@gmail.com <jerome.dewandre.mail@gmail.com>
2025-07-08 12:22:39 +0200

    add dbsource to view

--- odoo_etl/models/etl.py
+++ odoo_etl/models/etl.py
@@ -253,7 +253,7 @@ class ETLModel(models.Model):
             raise e
         
     @api.model
-    def load_records_in_batches(self, odoo_name, odoo_columns, batch_size=5000):
+    def load_records_in_batches(self, odoo_name, odoo_columns, batch_size=2000):
         """
         Load records in batches of a specified size and process them.
         
